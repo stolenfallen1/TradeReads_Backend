173a1ed84a7903e84e298634b15074f4543c670c
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2025-08-30 06:23:07 +0800

    set-up springboot

---
+++ src/main/java/com/tradereads/tradereads/TradereadsApplication.java
@@ -0,0 +1,13 @@
+package com.tradereads.tradereads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradereadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradereadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradereads/tradereads/TradereadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradereads.tradereads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradereadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
