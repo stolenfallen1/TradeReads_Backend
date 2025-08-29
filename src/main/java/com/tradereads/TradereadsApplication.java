2ac3d9ecabd8e47a6e3afd6a1095ff210d80be28
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2025-08-30 06:45:25 +0800

    Remove application.properties from remote repo

--- src/main/java/com/tradereads/TradereadsApplication.java
+++ src/main/java/com/tradereads/TradereadsApplication.java
@@ -1,4 +1,4 @@
-package com.tradereads.tradereads;
+package com.tradereads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tradereads/TradereadsApplicationTests.java
+++ src/test/java/com/tradereads/TradereadsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tradereads.tradereads;
+package com.tradereads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
