cfbd4a9364a58eebddb7761341f77bb0cecb3a70
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2025-08-30 09:46:53 +0800

    Book CRUD

---
+++ src/main/java/com/tradereads/service/BookService.java
@@ -0,0 +1,38 @@
+package com.tradereads.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.tradereads.model.Book;
+import com.tradereads.repository.BookRepository;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
